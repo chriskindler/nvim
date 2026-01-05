d5ef7f30c2283c302e0279eca799da3537d5ca43
kic04594 <christian.kindler@physik.uni-regensburg.de>
2026-01-05 08:39:34 +0100

    Last push to rqcd branch

--- lua/ck/plugins/colorscheme.lua
+++ lua/ck/plugins/colorscheme.lua
@@ -1,32 +1,12 @@
-return {
-  -- Lush is a required dependency for desolate
-  {
-    "rktjmp/lush.nvim",
-    lazy = true,
-  },
+vim.opt.termguicolors = true
+vim.opt.background = "dark"
+vim.cmd("colorscheme default")
 
-  -- Desolate colorscheme
-  {
-    "He4eT/desolate.nvim",
-    lazy = false,
-    priority = 1000,
-    dependencies = { "rktjmp/lush.nvim" },
-    config = function()
-      vim.opt.termguicolors = true
-      vim.opt.background = "dark"
-
-      -- Set colorscheme
-      vim.cmd("colorscheme desolate")
-
-      -- Force black background
-      vim.cmd("hi Normal guibg=#000000")
-
-      -- Optional: minimal highlight overrides
-      vim.cmd("hi Comment guifg=#444444")    -- muted blue-grey
-      vim.cmd("hi Identifier guifg=#c0caf5") -- light bluish white
-      vim.cmd("hi Function guifg=#7aa2f7")   -- soft blue
-      vim.cmd("hi Statement guifg=#bb9af7") 
-    vim.api.nvim_set_hl(0, "MatchParen", { bg = "#5f87ff", fg = "NONE", underline = false })
-    end,
-  },
-}
+for _, hl in ipairs({
+    "Normal",
+    "NormalFloat",
+    "SignColumn",
+    "EndOfBuffer",
+}) do
+  vim.api.nvim_set_hl(0, hl, { bg = "NONE" })
+end
